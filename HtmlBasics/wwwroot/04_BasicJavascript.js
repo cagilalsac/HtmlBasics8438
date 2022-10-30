a3466b196e5beba1f083245461676a0af8521876
Cagil ALSAC <cagil.alsac@bilgeadam.com>
2022-10-30 14:30:07 +0300

    v1

---
+++ HtmlBasics/wwwroot/04_BasicJavascript.js
@@ -0,0 +1,8 @@
+﻿function adSoyadBirlestir() {
+    var adTextBox = document.getElementById("tbAd");
+    var ad = adTextBox.value;
+    var soyad = document.getElementById("tbSoyad").value;
+    var adSoyadP = document.getElementsByClassName("sonuc")[0];
+    console.log(adTextBox);
+    adSoyadP.innerHTML = ad + " " + soyad;
+}
